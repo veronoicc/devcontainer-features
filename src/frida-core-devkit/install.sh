811ae95496c92a42db37e18ae16277b1d943d018
veronoicc <64193056+veronoicc@users.noreply.github.com>
2024-03-13 11:39:37 +0000

    Create parent directories if needed

--- src/frida-core-devkit/install.sh
+++ src/frida-core-devkit/install.sh
@@ -2,11 +2,11 @@
 set -e
 
 echo "Installing Frida Core devkit"
-echo "Downloading Frida Core devkit from $DOWNLOAD_URL"
 
 # Download the devkit
+echo "Downloading Frida Core devkit from $DOWNLOAD_URL"
 wget -O frida-core-devkit.tar.xz $DOWNLOAD_URL
 
 # Extract the devkit
-mkdir /usr/local/include/frida-core-devkit
+mkdir -p /usr/local/include/frida-core-devkit
 tar -xvf frida-core-devkit.tar.xz -C /usr/local/include/frida-core-devkit
