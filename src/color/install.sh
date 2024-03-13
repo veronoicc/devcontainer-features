f1517f4bc96ce403e7617e1fb7359e547a365488
veronoicc <64193056+veronoicc@users.noreply.github.com>
2024-03-13 07:42:34 +0000

    Add frida core devkit

--- src/color/install.sh
+++ src/color/install.sh
@@ -1,26 +1,12 @@
 #!/bin/sh
 set -e
+set +H
 
-echo "Activating feature 'color'"
-echo "The provided favorite color is: ${FAVORITE}"
+echo "Installing Frida Core devkit"
+echo "Downloading Frida Core devkit from $DOWNLOAD_URL"
 
+# Download the devkit
+wget -O frida-core-devkit.tar.xz $DOWNLOAD_URL
 
-# The 'install.sh' entrypoint script is always executed as the root user.
-#
-# These following environment variables are passed in by the dev container CLI.
-# These may be useful in instances where the context of the final 
-# remoteUser or containerUser is useful.
-# For more details, see https://containers.dev/implementors/features#user-env-var
-echo "The effective dev container remoteUser is '$_REMOTE_USER'"
-echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
-
-echo "The effective dev container containerUser is '$_CONTAINER_USER'"
-echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
-
-cat > /usr/local/bin/color \
-<< EOF
-#!/bin/sh
-echo "my favorite color is ${FAVORITE}"
-EOF
-
-chmod +x /usr/local/bin/color
+# Extract the devkit
+tar -xvf frida-core-devkit.tar.xz -C /usr/local/include/frida-core-devkit
